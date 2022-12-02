cf4e37ada0c373d1f8ba69f95573b09a817489cb
MysteryCoder456 <rehatbir.singh@gmail.com>
2022-12-02 19:37:54 +0400

    day 1 part 2

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -2,7 +2,7 @@ use input::input::get_input;
 
 fn main() {
     let input = get_input(1);
-    let mut max_calories = 0;
+    let mut top_elves = [0, 0, 0];
 
     input.split("\n\n").for_each(|elf| {
         let mut elf_calories = 0;
@@ -13,10 +13,21 @@ fn main() {
                 Err(_) => {}
             });
 
-        if elf_calories > max_calories {
-            max_calories = elf_calories;
+        if elf_calories > top_elves[0] {
+            top_elves[2] = top_elves[1];
+            top_elves[1] = top_elves[0];
+            top_elves[0] = elf_calories;
+        } else if elf_calories > top_elves[1] {
+            top_elves[2] = top_elves[1];
+            top_elves[1] = elf_calories;
+        } else if elf_calories > top_elves[2] {
+            top_elves[2] = elf_calories;
         }
     });
 
-    println!("{max_calories}");
+    println!("Calories by top elf: {}", top_elves[0]);
+    println!(
+        "Total calories of top 3 elves: {}",
+        top_elves[0] + top_elves[1] + top_elves[2]
+    );
 }
